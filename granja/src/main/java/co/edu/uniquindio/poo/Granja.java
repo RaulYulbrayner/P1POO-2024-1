d9e37937704ab20217c6c4521c4765c365f1d749
RaulYulbrayner <153127665+RaulYulbrayner@users.noreply.github.com>
2024-05-09 20:32:51 -0500

    update

--- granja/src/main/java/co/edu/uniquindio/poo/Granja.java
+++ granja/src/main/java/co/edu/uniquindio/poo/Granja.java
@@ -1,25 +1,79 @@
 package co.edu.uniquindio.poo;
 
+import java.util.Scanner;
+
 public class Granja {
     
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Animal[] animales = new Animal[5];
-        animales[0] = new Perro();
-        animales[1] = new Gato();
-        animales[2] = new Pato();
-        animales[3] = new Perro();
-        animales[4] = new Pato();
+        int opcion = 0;
+        while (opcion != 8) {
+            System.out.println("1. Añadir Animal");
+            System.out.println("2. Mostrar todos los Animales");
+            System.out.println("3. Hacer sonidos");
+            System.out.println("4. Información de un animal");
+            System.out.println("5. Dormir");
+            System.out.println("6. Comer");
+            System.out.println("7. Eliminar Animal");
+            System.out.println("8. Salir");
+            System.out.print("Ingrese una opción: ");
+            opcion = scanner.nextInt();
 
-        //   0     1      2     3      4
-        //[Perro, Gato, Pato, Perro, Pato]
-
-        for (Animal animal : animales) {
-            //2
-            animal.hacerSonido();
+            switch (opcion) {
+                case 1:
+                    Animal.agregarAnimal();
+                    break;
+                case 2:
+                    Animal.mostrarAnimales();
+                    break;
+                case 3:
+                    hacerSonido();
+                    break;
+                case 4:
+                    Animal.mostrarInfoAnimal();
+                    break;
+                case 5:
+                    dormir();
+                    break;
+                case 6:
+                    comer();
+                    break;
+                case 7:
+                    Animal.eliminarAnimal();
+                    break;
+                case 8:
+                    System.out.println("Chao muchachos...");
+                    break;
+            }
         }
-        
+        scanner.close();
     }
 
+    /**
+     * Metodo que permite saber el sonido que hace un animal
+     */
+    private static void hacerSonido() {
+        for (Animal animal : Animal.getAnimales()) {
+            animal.hacerSonido();
+        }
+    }
 
+    /**
+     * Metodo que permite saber como duerme un animal
+     */
+    private static void dormir() {
+        for (Animal animal : Animal.getAnimales()) {
+            animal.dormir();
+        }
+    }
 
+    /**
+     * Metodo que permite saber como come un animal
+     */
+    private static void comer() {
+        for (Animal animal : Animal.getAnimales()) {
+            animal.comer();
+        }
+    }
 }
--- granja/src/main/java/co/edu/uniquindio/poo/Perro.java
+++ granja/src/main/java/co/edu/uniquindio/poo/Perro.java
@@ -1,10 +1,39 @@
 package co.edu.uniquindio.poo;
 
-public class Perro extends  Animal {
+public class Perro extends Animal {
 
+    /**
+     * Metodo constructor de la clase Perro
+     * @param nombre
+     * @param color
+     * @param edad
+     */
+    public Perro(String nombre, String color, int edad){
+        super(nombre, color, edad);
+    }
+
+    /**
+     * Metodo que define el sonido de un perro 
+     */
     @Override
     public void hacerSonido(){
-        System.out.println("Guau Guau Guau");
+        System.out.println("Guau Guau");
     }
-    
+
+    /**
+     * Metodo que define como duerme un perro
+     */
+    @Override
+    public void dormir() {
+        System.out.println("El Guau Guau esta durmiendo zZzZzZ");
+    }
+
+    /**
+     * Metodo que define como come un perro
+     */
+    @Override
+    public void comer() {
+        System.out.println("El Guau Guau esta comiendo");
+    }
+
 }
